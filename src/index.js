8e49f84e8d65715d0231ed001a3b24052a4a170a
Tomioka25 <laladko@mail.ru>
2022-08-08 16:44:25 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let s = String(n);
+  let result = '';
+
+  if (n < 0){
+    s = String(-n);
+  }
+
+  for (let i = 0; i < s.length; i++){
+    result = `${s[i]}${result}`
+  }
+  return Number(result);
 }
